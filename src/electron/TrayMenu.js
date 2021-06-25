a00a9353541b8e940dccc5b47d31e1612377ac74
Daniel Lemper <ich@dlemper.de>
2021-06-26 00:20:37 +0200

    add tray menu

---
+++ src/electron/TrayMenu.js
@@ -0,0 +1,37 @@
+import { app, Tray, Menu, nativeImage } from 'electron';
+
+export class TrayMenu {
+  constructor() {
+    this.iconPath = '/assets/camera.png';
+    this.tray = new Tray(this.createNativeImage());
+  }
+
+  createNativeImage() {
+    // Since we never know where the app is installed,
+    // we need to add the app base path to it.
+    const path = `${app.getAppPath()}${this.iconPath}`;
+    const image = nativeImage.createFromPath(path);
+    // Marks the image as a template image.
+    image.setTemplateImage(true);
+    return image;
+  }
+
+  createMenu() {
+    // This method will create the Menu for the tray
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Tokei',
+        type: 'normal',
+        click: () => { 
+          /* Later this will open the Main Window */ 
+        }
+      },
+      {
+        label: 'Quit',
+        type: 'normal',
+        click: () => app.quit()
+      }
+    ]);
+    return contextMenu;
+  }
+}
