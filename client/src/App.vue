eab528e8ae6e0a48ebbd92faaf9f706713145ea4
Daniel Lemper <ich@dlemper.de>
2021-06-26 14:00:26 +0200

    fine tune ui

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <b-container>
+  <b-container style="max-width: 34rem">
     <b-row>
       <b-col>
-        <b-form @submit="onSubmit" @reset="onReset" ref="form">
+        <b-form @submit.prevent="onSubmit" @reset="onReset" ref="form">
           <b-form-group label="Auftragsnummer:" label-for="order">
             <b-form-input
               v-model="orderId"
@@ -28,18 +28,39 @@
             </b-form-file>
           </b-form-group>
 
-          <b-card class="mb-3 text-center">
-            <img-file :file="file" v-if="file"></img-file>
-            <b-badge pill variant="light">{{ orderId }}</b-badge>
+          <b-card
+            class="mb-3 text-center"
+            style="min-height: calc(100vh - 16rem)"
+          >
+            <img-file
+              :file="file"
+              v-if="file"
+              style="max-height: calc(100vh - 20rem)"
+            ></img-file>
+            <br />
+            <b-badge pill variant="light" v-show="orderId"
+              >{{ orderId }}.jpg</b-badge
+            >
           </b-card>
 
           <b-form-row class="justify-content-between mb-3">
-            <b-col class="flex-grow-0">
-              <b-button type="reset" variant="danger">Abbrechen</b-button>
+            <b-col>
+              <b-button type="reset" variant="danger">
+                <b-icon icon="x" />
+                Abbrechen
+              </b-button>
             </b-col>
 
-            <b-col class="flex-grow-0">
-              <b-button type="submit" variant="primary">Speichern</b-button>
+            <b-col class="text-right">
+              <b-button
+                type="submit"
+                variant="primary"
+                :disabled="!file || !orderId"
+              >
+                <b-icon icon="upload" v-show="!isUploading" />
+                <b-spinner small v-show="isUploading" />
+                Speichern
+              </b-button>
             </b-col>
           </b-form-row>
         </b-form>
@@ -56,15 +77,36 @@ export default {
     return {
       orderId: "",
       file: null,
+      isUploading: false,
     };
   },
 
   methods: {
-    onSubmit($event) {
-      console.log("test", $event);
+    onSubmit() {
+      if (!this.file || !this.orderId) {
+        return;
+      }
+
+      this.isUploading = true;
+
+      const formData = new FormData();
+      formData.append(
+        "file",
+        new File([this.file], `${this.orderId}.jpg`, { type: this.file.type })
+      );
+
+      await fetch("/api/file", {
+        method: "POST",
+        body: formData,
+      });
+
+      this.isUploading = false;
     },
 
-    onReset() {},
+    onReset() {
+      this.orderId = "";
+      this.isUploading = false;
+    },
   },
 };
 </script>
