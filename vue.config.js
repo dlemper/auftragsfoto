b1314677900e34d20fa40b1b964f88e020c3ca2d
Daniel Lemper <ich@dlemper.de>
2021-06-27 15:58:49 +0200

    add client dist to electron build

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,20 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new CopyPlugin({
-        patterns: [{ from: "src/assets", to: "assets" }],
+        patterns: [
+          { from: "src/assets", to: "assets" },
+          { from: "client/dist", to: "client/dist" }
+        ],
       }),
     ],
   },
   pluginOptions: {
     electronBuilder: {
+      externals: ['formidable'],
       chainWebpackMainProcess: (config) => {
         // Chain webpack config for electron main process only
         config.externals({
-            formidable: 'commonjs formidable',
+          formidable: 'commonjs formidable',
         });
       },
     },
