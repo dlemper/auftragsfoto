95f9788083f985c36707901b17c5710d0e89f496
Daniel Lemper <ich@dlemper.de>
2021-07-07 21:50:49 +0200

    prettier

--- src/electron/httpserver.js
+++ src/electron/httpserver.js
@@ -1,37 +1,43 @@
-const finalhandler = require('finalhandler');
-const http = require('http');
-const serveStatic = require('serve-static');
-const formidable = require('formidable');
-const path = require('path');
-const mv = require('mv');
+const finalhandler = require("finalhandler");
+const http = require("http");
+const serveStatic = require("serve-static");
+const formidable = require("formidable");
+const path = require("path");
+const mv = require("mv");
 
-const { getPath } = require('./getpath');
+const { getPath } = require("./getpath");
 
-const serve = serveStatic(path.join(__dirname, 'client', 'dist'), { 'index': ['index.html', 'index.htm'] });
-
-const fromParse = (req) => new Promise((resolve, reject) => {
-  const form = formidable();
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve({ fields, files });
-    }
-  });
+const serve = serveStatic(path.join(__dirname, "client", "dist"), {
+  index: ["index.html", "index.htm"],
 });
 
-const mvClob = (from, to) => new Promise((resolve, reject) => mv(from, to, { clobber: false }, (err) => {
-  if (err) {
-    reject(err);
-  } else {
-    resolve();
-  }
-}));
+const fromParse = (req) =>
+  new Promise((resolve, reject) => {
+    const form = formidable();
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ fields, files });
+      }
+    });
+  });
+
+const mvClob = (from, to) =>
+  new Promise((resolve, reject) =>
+    mv(from, to, { clobber: false }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  );
 
 // Create server
 const server = http.createServer(async function onRequest(req, res) {
-  if (req.url === '/api/upload' && req.method.toLowerCase() === 'post') {
+  if (req.url === "/api/upload" && req.method.toLowerCase() === "post") {
     try {
       const { files } = await fromParse(req);
       const uploadDir = await getPath();
@@ -50,14 +56,14 @@ const server = http.createServer(async function onRequest(req, res) {
       }
 
       res.write(e.message);
-      res.end()
+      res.end();
     }
 
     return;
   }
 
-  serve(req, res, finalhandler(req, res))
-})
+  serve(req, res, finalhandler(req, res));
+});
 
 // Listen
-server.listen(3000)
+server.listen(3000);
